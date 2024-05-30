e5cb4d165608c51937db7801edc2b2d3fa795ec5
UmbraServus <ksprague0592@gmail.com>
2024-05-30 17:17:30 -0500

    tested base_model with main; added attributes name and my number; fixed attributes id and datetimes so that they are assigned correctly using if else statments; fixed save method it was forgetting a datetime. added to the beggining of the method being called.

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ module: base model class """
 import datetime
+import uuid
 
 class BaseModel:
     """ BaseModel class that is to be inherited by other models
@@ -9,27 +10,33 @@ class BaseModel:
             id(str): uuid for unique Identifier
             created_at(str): time it was created
             updated_at(str): time instance was updated
+            name(str): name of instance
+            my_number(int): number given to instance
     """
-    def __init__(self, id=None, created_at, updated_at):
+    def __init__(self, name=None, my_number=0, created_at=None, updated_at=None, id=None):
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.name = name
+        self.my_number = my_number
+        self.id = id if id else str(uuid.uuid4())
+        self.created_at = created_at if created_at else datetime.datetime.now().isoformat()
+        self.updated_at = updated_at if updated_at else datetime.datetime.now().isoformat()
 
     """ methods/setters/getters """
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     
     def save(self):
         """updates updated_at attribute"""
 
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
 
     def to_dict(self):
         """ returns dictionary keyword/value representations of attributes"""
 
         return {
+            "name": self.name,
+            "my_number": self.my_number,
             "id": self.id,
             "created_at": self.created_at,
             "updated_a t": self.updated_at,
