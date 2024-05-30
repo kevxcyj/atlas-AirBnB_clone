22add0544dcdd119a2b3e038b4fe932d8a18a7ff
kevxcyj <kevcyjunior@gmail.com>
2024-05-30 07:32:05 +0000

    Added If statement to rewrite init method, this also converts the datetime into the correct format

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,48 @@
+#!/usr/bin/python3
+""" module: base model class """
 import datetime
 
-
-
 class BaseModel:
+    """ BaseModel class that is to be inherited by other models
 
-self.id = str(uuid.uuid4())
-self.created_at = datetime.datetime.now().isoformat()
-self.updated_at = datetime.datetime.now().isoformat()
-
-
-
-
-def __init__(self, id=none, created_at, updated_at):
-    pass
-
-
-def __str__(self):
-    pass
-
-def save(self)
-self.updated_at = datetime.now().isoformat()
-
-
-
-
-if kwargs:
+        Attribute:
+            id(str): uuid for unique Identifier
+            created_at(str): time it was created
+            updated_at(str): time instance was updated
+    """
+    def __init__(self, id=None, created_at, updated_at):
+        
+        if kwargs:
     excluded_keys = ['__class__']
     for key, value in kwargs,items():
             if key not in excluded_keys:
                 if isinstance(value, str) and (key == 'created_at' or key == 'updated_at'):
                     value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
             setattr(self, key, value)
+            else
 
-        else 
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
 
-            
+    """ methods/setters/getters """
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict}"
+    
+    def save(self):
+        """updates updated_at attribute"""
+
+        self.updated_at = datetime.now().isoformat()
+
+    def to_dict(self):
+        """ returns dictionary keyword/value representations of attributes"""
+
+        return {
+            "id": self.id,
+            "created_at": self.created_at,
+            "updated_a t": self.updated_at,
+            "__class__": self.__class__.__name__,
+
+        }
+
