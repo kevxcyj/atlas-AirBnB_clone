50db5d30b3527a11a53559e738dace22eb218e9b
UmbraServus <ksprague0592@gmail.com>
2024-05-30 22:55:35 -0500

    fixed the datetime issues of when asked for string rep format vs dictionary representation in iso.

--- models/base_model.py
+++ models/base_model.py
@@ -20,14 +20,13 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key not in excluded_keys:
                     if isinstance(value, str) and (key == 'created_at' or key == 'updated_at'):
-                        value = datetime.fromisoformat(value)
+                        value = datetime.datetime.fromisoformat(value)
                     setattr(self, key, value)
 
         else:
 
                     self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now().isoformat()
-                    self.updated_at = None
+                    self.created_at = datetime.datetime.now()
 
     """ methods/setters/getters """
 
@@ -37,7 +36,7 @@ class BaseModel:
     def save(self):
         """updates updated_at attribute"""
 
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """ returns dictionary keyword/value representations of attributes"""
@@ -46,8 +45,8 @@ class BaseModel:
             "name": self.name,
             "my_number": self.my_number,
             "id": self.id,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
             "__class__": self.__class__.__name__,
 
         }
