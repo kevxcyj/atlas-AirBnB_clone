bb447f5690cf80ce20b3d90a208e1e22b5287c9c
kevxcyj <kevcyjunior@gmail.com>
2024-06-04 21:38:20 +0000

    Removed import review

--- console.py
+++ console.py
@@ -7,7 +7,6 @@ from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.review import Review
 from models.amenity import Amenity
 from models.place import Place
 import models
