01c3cf68176ea9b5a990e51fa1e8e51106f7d01d
UmbraServus <ksprague0592@gmail.com>
2024-06-04 14:45:44 -0500

    added user  class

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" user class module """
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ class User inheriting from BaseModel
+
+        attributes:
+            email(str): email of a user
+            password(str): password of a user
+            first_name(str): first name of a user
+            last_name(str): last name of a user
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
