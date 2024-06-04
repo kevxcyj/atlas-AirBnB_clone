5c469d061ee7be5a257f9b0ddfb937e263047a31
kevxcyj <kevcyjunior@gmail.com>
2024-06-04 07:38:22 +0000

    Added create function to console

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Contains the entry point of the command interperter """
 import cmd
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
@@ -16,6 +17,19 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """" Command to exit the program on EOF """
         return True
+
+    def do_create(self, line):
+        args = line.split()
+        if len(args)!= 1:
+            print("** class name missing **")
+            return
+        className = args[0]
+        if className not in storage.classes:
+            print("** class doesn't exist **")
+            return
+        obj = storage.new(className)
+        obj.save()
+        print(obj.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
