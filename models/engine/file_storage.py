1319ea7a9744c0da7cd3f61621729db0b491e962
kevxcyj <kevcyjunior@gmail.com>
2024-09-30 13:28:00 -0500

    Created file storage and added import statement to init

---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+    
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        objects_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, "w") as f:
+            json.dump(objects_dict, f)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, "r") as f:
+                objects_dict = json.load(f)
+                for key, obj_dict in objects_dict.items():
+                    cls = eval(obj_dict['__class__'])
+                    obj = cls(**obj_dict)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
