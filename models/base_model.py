8925278d12cf52d6ea2dbb2abe065846ff8c3795
kevxcyj <kevcyjunior@gmail.com>
2024-09-30 00:09:31 -0500

    Created base model class and save/to_dict methods

---
+++ models/base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import uuid
+import datetime
+
+
+class BaseModel
+
+def __init__(self):
+    self.id = str(uuid.uuid4())
+    self.created_at = datetime.now().isoformat()
+    self.updated_at = datetime.now().isoformat()
+
+
+def save(self):
+    self.updated_at = datetime.now().isoformat()
+
+def to_dict(self):
+    return {
+        "__class__": self.__class__.__name__,
+        **self.__dict__
+    }
