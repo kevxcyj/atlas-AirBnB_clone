655a6e5ef6e177f69f6954015de8854e92e08a43
kevxcyj <kevcyjunior@gmail.com>
2024-10-01 12:02:22 -0500

    Added create method

--- console.py
+++ console.py
@@ -7,11 +7,27 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
 
+    def do_create(self, arg):
+        try:
+            cls = eval(arg)
+            obj = cls()
+            storage.new(obj)
+            storage.save()
+            print(f"{obj.id}")
+        except NameError:
+            print("** class doesn't exist **")
+        except Exception as e:
+            print(f"{e}")
+
+
+
     def do_EOF(self, arg):
+        """ End of file """
         print()
         return True
     
     def do_quit(self, arg):
+        """ Quit Command """
         return True
     
     def postcmd(self, stop, arg):
