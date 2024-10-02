f8f54bd40128766d4c7176aa3b5d4a209d76c660
kevxcyj <kevcyjunior@gmail.com>
2024-10-01 20:50:18 -0500

    Wrong file

--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -1,52 +1,16 @@
-import unittest
-from unittest.mock import patch
-from io import StringIO
-from models.engine.file_storage import FileStorage
+#!/usr/bin/python3
+from models import storage
+from models.base_model import BaseModel
 
-class TestFileStorage(unittest.TestCase):
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
 
-    def setUp(self):
-        self.fs = FileStorage()
-
-    def test_new(self):
-        self.fs.new(FileStorage())
-        self.assertIn('FileStorage', self.fs.__objects.keys())
-
-    def test_save(self):
-        with patch('builtins.open', new_callable=StringIO) as mock_open:
-            self.fs.new(FileStorage())
-            self.fs.save()
-            mock_open.assert_called_once_with('file.json', 'w')
-
-    def test_reload(self):
-        with patch('builtins.open', new_callable=StringIO) as mock_open:
-            mock_open.return_value.write.return_value = '{"FileStorage": {"id": "123"}}'
-            self.fs.reload()
-            self.assertEqual(len(self.fs.__objects), 1)
-
-    def test_all(self):
-        self.fs.new(FileStorage())
-        self.fs.all()
-        self.assertEqual(len(self.fs.__objects), 1)
-
-    def test_new_existing_object(self):
-        obj = FileStorage()
-        self.fs.new(obj)
-        self.fs.new(obj)
-        self.assertEqual(len(self.fs.__objects), 1)
-
-    def test_save_empty_file(self):
-        with patch('builtins.open', new_callable=StringIO) as mock_open:
-            mock_open.return_value.read.return_value = '{}'
-            self.fs.reload()
-            self.fs.save()
-            mock_open.assert_called_with('file.json', 'w')
-
-    def test_reload_nonexistent_file(self):
-        with patch('builtins.open', new_callable=StringIO) as mock_open:
-            mock_open.side_effect = FileNotFoundError
-            self.fs.reload()
-            self.assertEqual(len(self.fs.__objects), 0)
-
-if __name__ == '__main__':
-    unittest.main()
+print("-- Create a new object --")
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model.save()
+print(my_model)
