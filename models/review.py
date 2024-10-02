3226f135b2673149340af3b9fcb67d1e99fa1714
Rain Nyte <rain_nyte@proton.me>
2024-10-01 19:03:45 -0500

    created State, City, Amenity, Place, and Review classes for use with the console and file storage modules

--- models/review.py
+++ models/review.py
@@ -1,10 +1,9 @@
 from models.base_model import BaseModel
 
-class User(BaseModel):
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+class Review(BaseModel):
+        place_id = ''
+        user_id = ''
+        text = ''
         
         def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
