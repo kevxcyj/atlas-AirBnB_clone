7cdfa5a7ae59e5ba6eec925d3098a153e3707315
kevxcyj <kevcyjunior@gmail.com>
2024-10-02 09:26:11 -0500

    Added comments to file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" Initalizing file_storage """
 import json
 import os
 from models.base_model import BaseModel
@@ -7,22 +7,33 @@ from models.user import User
 
 
 class FileStorage:
+    """ Class for serializing instances to a JSON file 
+    
+    Attributes:
+        __file_path (str): Path to JSON where files start
+        __objects (dict): Dictionary containing instanced objects
+    
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """ Returns dictionary objects """
         return self.__objects
     
     def new(self, obj):
+        """ Adds new object to dictionary """
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
+        """ Serializes object to JSON file """
         objects_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, "w") as f:
             json.dump(objects_dict, f)
 
     def reload(self):
+        """ Deserializes JSON file """
         try:
             with open(self.__file_path, "r") as f:
                 objects_dict = json.load(f)
